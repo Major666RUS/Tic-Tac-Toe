a83af4ae6ca4a95c7a63f9c511f0765c4602254c
Vadim Falin <VFalin@domain.corp>
2019-11-18 18:13:55 +0300

    fix build

---
+++ build/precache-manifest.e40cddbb3b45b740f57f1dc79cd16f83.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d226010e710ae3b59cb4b5168f21b92e",
+    "url": "https://major666rus.github.io/Tic-Tac-Toe/build/index.html"
+  },
+  {
+    "revision": "5a59edd73e1dbf1b88b2",
+    "url": "https://major666rus.github.io/Tic-Tac-Toe/build/static/css/main.0ebde3ef.chunk.css"
+  },
+  {
+    "revision": "8a83808158163cc96714",
+    "url": "https://major666rus.github.io/Tic-Tac-Toe/build/static/js/2.a7f526f7.chunk.js"
+  },
+  {
+    "revision": "5a59edd73e1dbf1b88b2",
+    "url": "https://major666rus.github.io/Tic-Tac-Toe/build/static/js/main.c00e1245.chunk.js"
+  },
+  {
+    "revision": "5ff2cdd6c52abe2038e4",
+    "url": "https://major666rus.github.io/Tic-Tac-Toe/build/static/js/runtime-main.f91a670c.js"
+  }
+]);
